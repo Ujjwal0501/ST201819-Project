a14265435ced7e51c15aebe15de271190e7001c0
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-07 14:49:26 +0530

    store login state of user

--- js/login.js
+++ js/login.js
@@ -2,16 +2,22 @@ function local_login () {
 
 	var uname = document.getElementsByName("user-name")[0].value;
 	var pass = document.getElementsByName('password')[0].value;
-	console.log(''+uname+pass);
+	
 	if (uname == 'admin' && pass == 'admin123') {
 		// redirect to user page
+		save_user(uname, pass);
 		alert("Admin Login Succussful!");
 		window.open("../index.html", '_top');
 	} else if (uname == 'user' && pass == 'user123') {
 		// redirect to admin page
+		save_user(uname, pass);
 		alert("User Login Succussful!");
 		window.open("../index.html", '_top');
 	} else {
 		alert("Invalid user credentials!!");
 	}
+}
+
+function save_user (uname, pass) {
+	sessionStorage[uname] = "loggedin";
 }
