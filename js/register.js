9bc1ccb9198f3484014f09feedaa3dccab56dd32
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-07 19:16:18 +0530

    registration with empty value not allowed

--- js/register.js
+++ js/register.js
@@ -1,4 +1,12 @@
 function local_registration () {
-	alert("You are now registered!");
-	window.open('./login.html', '_top');
+	var val = "You are now registered!"
+	if (document.getElementsByName("full-name")[0].value == "")
+		val = "Invalid input/no input";
+	else if (document.getElementsByName("user-name")[0].value == "")
+		val = "Invalid input/no input";
+	else if (document.getElementsByName("password")[0].value == "")
+		val = "Invalid input/no input";
+	alert(val);
+	if (val != "Invalid input/no input")
+		window.open("../webpages/login.html", '_top');
 }
