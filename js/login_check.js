c7f1a167f97c75addb62a27d4314743e7fb1c6f5
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-08 14:22:54 +0530

    changed nav options

--- js/login_check.js
+++ js/login_check.js
@@ -1,7 +1,13 @@
 // import this script in a page to check log-in state and
 // redirect to index.html if the usr is logged out
 
-if (sessionStorage["admin"] != "loggedin" && sessionStorage["user"] != "loggedin") {
-	// redirect to index.html if user is not logged in
-	window.open("../index.html", '_top');
+function login_check () {
+	if (sessionStorage["admin"] != "loggedin" && sessionStorage["user"] != "loggedin") {
+		
+		document.getElementById("logout").style.display = 'none';
+		document.getElementById("login").style.display = 'block';
+	} else {
+		document.getElementById("logout").style.display = 'block';
+		document.getElementById("login").style.display = 'none';
+	}
 }
