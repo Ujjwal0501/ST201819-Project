8c483b49811e063ad5ec0fd271c8727a17f6a4c4
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-09 02:31:57 +0530

    add sell page

---
+++ js/sell.js
@@ -0,0 +1,10 @@
+function local_sell () {
+	var id = 7;
+	var name = document.getElementsByName('property-name')[0].value;
+	var location = document.getElementsByName('property-location')[0].value;
+	var price = document.getElementsByName('property-price')[0].value;
+	var sname = document.getElementsByName('seller-name')[0].value;
+	sessionStorage["7"] = [""+id, name, location, price, sname];
+	alert("Your property has been listed for selling with provided details.\nHang on till someone buys it!!")
+	console.log(sessionStorage["7"]);
+}
