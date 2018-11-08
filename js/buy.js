7d760a0c0021739d6d7c89c7a91ff6d5c9d0be05
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-09 04:38:35 +0530

    show details provided through form

--- js/buy.js
+++ js/buy.js
@@ -12,11 +12,17 @@ function buy_local (propertyid) {
 }
 
 function hide_sold () {
-	for (var i = 1; i <= 10; i++) {
+	for (var i = 1; i <= 16; i++) {
 		if (sessionStorage[i] == "sold") {
 			console.log(i+" sold\n");
 			document.getElementById(''+i).style.display = 'none';
 		} else if (sessionStorage[i] == "added") {
+			var arr = sessionStorage["ID"+i].toString().split(",");
+			document.getElementById('name'+i).innerHTML = arr[0];
+			document.getElementById('loc'+i).innerHTML = arr[1];
+			document.getElementById('price'+i).innerHTML = arr[2];
+			document.getElementById('sname'+i).innerHTML = arr[3];
+
 			document.getElementById(''+i).style.display = 'table-row';
 		}
 	}
--- js/sell.js
+++ js/sell.js
@@ -2,13 +2,15 @@
 var id = 8;
 
 function local_sell () {
+	if (sessionStorage["idd"]) id = Number(String(sessionStorage["idd"]));
 	id++;
 	var name = document.getElementsByName('property-name')[0].value;
 	var location = document.getElementsByName('property-location')[0].value;
 	var price = document.getElementsByName('property-price')[0].value;
 	var sname = document.getElementsByName('seller-name')[0].value;
-	sessionStorage["ID9"] = [""+id, name, location, price, sname];
+	sessionStorage["ID"+id] = [name+"", location+"", price+"", sname+""];
 	sessionStorage[id] = "added";
+	sessionStorage["idd"] = id;
 	alert("Your property has been listed for selling with provided details.\nHang on till someone buys it!!")
 	console.log(sessionStorage[id]);
 }
