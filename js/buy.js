27ec4761fbae46f38af5b8b477fac54c05c243c1
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2018-11-11 23:55:25 +0530

    added functionality in report page

--- js/buy.js
+++ js/buy.js
@@ -1,10 +1,3 @@
-function show_detail () {
-	if (sessionStorage["admin"] != "loggedin" && sessionStorage["user"] != "loggedin") {
-		alert("You are not logged in!!\nThis feature requires a login account.");
-	}
-	else
-		window.open("./property_detail.html", '_top');
-}
 
 function buy_local (propertyid) {
 	if (sessionStorage["admin"] != "loggedin" && sessionStorage["user"] != "loggedin") {
